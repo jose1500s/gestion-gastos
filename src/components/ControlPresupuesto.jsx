53e8fdbaf62ac9277e10d745467c76bd38645dd7
Jose Antonio <confi15826s@gmail.com>
2023-12-31 02:17:38 -0600

    tremor instalado, avance en el dashboard principal

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,5 +1,22 @@
-export default function ControlPresupuesto({ presupuesto }) {
+import { Card, Text, Metric, DonutChart, Title, Button } from "@tremor/react";
 
+export default function ControlPresupuesto({ presupuesto }) {
+    const cities = [
+        {
+            name: "New York",
+            sales: 9800,
+        },
+        {
+            name: "London",
+            sales: 4567,
+        },
+        {
+            name: "Hong Kong",
+            sales: 3908,
+        },
+    ];
+
+    const valueFormatter = (number) => `$ 300`
     const formatearPresupuesto = presupuesto => {
         return presupuesto.toLocaleString('en-US', {
             style: 'currency',
@@ -7,11 +24,48 @@ export default function ControlPresupuesto({ presupuesto }) {
         })
     }
 
-    return(
-        <>
-            <h2 className="text-5xl text-white">
-                presupuesto: {formatearPresupuesto(presupuesto)}
-            </h2>
-        </>
+    return (
+        <div className="w-5/6 flex gap-5">
+            <section id="resumen" className="w-9/12 p-5 h-56 bg-slate-800 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm ">
+                <header className="flex justify-between">
+                    <div>
+                        <h2 className="text-2xl text-white font-semibold ">Resumen General</h2>
+                    </div>
+                    <div>
+                        <button className="p-2 bg-white rounded-md">Agregar gasto +</button>
+                    </div>
+                </header>
+                <section className="flex justify-between items-center pt-8">
+                    <Card className="max-w-xs">
+                        <Text>Presupuesto</Text>
+                        <Metric className="pt-1">{formatearPresupuesto(presupuesto)}</Metric>
+                    </Card>
+
+                    <Card className="max-w-xs">
+                        <Text>Disponible</Text>
+                        <Metric className="pt-1">$ 200.00</Metric>
+                    </Card>
+
+                    <Card className="max-w-xs">
+                        <Text>Gastado</Text>
+                        <Metric className="pt-1">$ 300,00</Metric>
+                    </Card>
+                </section>
+            </section>
+
+            <section className="w-3/12 p-5 bg-slate-800 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm">
+                <Card className="max-w-lg">
+                    <Title>Gastos:</Title>
+                    <DonutChart
+                        className="mt-6"
+                        data={cities}
+                        category="sales"
+                        index="name"
+                        valueFormatter={valueFormatter}
+                        colors={["slate", "violet", "indigo", "rose", "cyan", "amber"]}
+                    />
+                </Card>
+            </section>
+        </div>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,7 @@ import ControlPresupuesto from "./ControlPresupuesto"
 
 export default function Header({ presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto }) {
     return (
-        <div className="absolute inset-0 -z-10 h-full w-full items-center pt-9 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#1c357df7_100%)]">
-            <header className="flex flex-col  items-center h-full gap-10">
+            <header className="flex flex-col  items-center h-screen gap-10 bg-gradient-to-b from-gray-900 to-gray-600 ">
                 <h1
                     className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-[2.85rem] text-center"
                 >
@@ -27,6 +26,5 @@ export default function Header({ presupuesto, setPresupuesto, isValidPresupuesto
 
 
             </header>
-        </div>
     )
 }
