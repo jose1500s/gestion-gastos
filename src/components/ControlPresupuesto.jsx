e2df8b34941afa479bfa4a61b402a47b96885de3
Jose Antonio <confi15826s@gmail.com>
2024-01-01 23:38:36 -0600

    responsive mobile

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -44,36 +44,44 @@ export default function ControlPresupuesto({ presupuesto }) {
 
     return (
         <>
-            <div className="md:w-full px-6 md:flex gap-5">
-                <section id="resumen" className="w-full p-5 lg:w-9/12  h-auto bg-slate-800 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm ">
-                    <header className="flex justify-between">
-                        <div>
-                            <h2 className="text-2xl text-white font-semibold ">Resumen General</h2>
-                        </div>
-                        <div>
-                            <ModalNuevoGasto
-                                guardarGasto={guardarGasto}
-                            />
-                        </div>
-                    </header>
-                    <section className="flex justify-between flex-wrap  items-center pt-8">
-                        <Card className="lg:max-w-xs m-1">
-                            <Text>Presupuesto</Text>
-                            <Metric className="pt-1">{formatearPresupuesto(presupuesto)}</Metric>
-                        </Card>
+            <section id="resumen" className="w-full p-5 lg:w-4/5  h-auto bg-slate-800 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm ">
+                <header className="flex justify-between">
+                    <div>
+                        <h2 className="text-2xl text-white font-semibold ">Resumen General</h2>
+                    </div>
+                    <div>
+                        <ModalNuevoGasto
+                            guardarGasto={guardarGasto}
+                        />
+                    </div>
+                </header>
+                <section className="flex justify-between flex-wrap  items-center pt-8">
+                    <Card className="lg:max-w-xs m-1">
+                        <Text>Presupuesto</Text>
+                        <Metric className="pt-1">{formatearPresupuesto(presupuesto)}</Metric>
+                    </Card>
 
-                        <Card className="lg:max-w-xs m-1">
-                            <Text>Disponible</Text>
-                            <Metric className="pt-1">$ 200.00</Metric>
-                        </Card>
+                    <Card className="lg:max-w-xs m-1">
+                        <Text>Disponible</Text>
+                        <Metric className="pt-1">$ 200.00</Metric>
+                    </Card>
 
-                        <Card className="lg:max-w-xs m-1">
-                            <Text>Gastado</Text>
-                            <Metric className="pt-1">$ 300,00</Metric>
-                        </Card>
-                    </section>
+                    <Card className="lg:max-w-xs m-1">
+                        <Text>Gastado</Text>
+                        <Metric className="pt-1">$ 300,00</Metric>
+                    </Card>
                 </section>
+            </section>
 
+            <div className='md:flex w-4/5'>
+                <main className='w-5/6'>
+                    <div className='w-[73%]'>
+                        <ListadoGastos
+                            gastos={gastos}
+                        />
+                    </div>
+                    <span className='hidden' />
+                </main>
                 <section className="w-full lg:w-3/12 p-5 mt-2 bg-slate-800 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm">
                     <Card className="max-w-lg">
                         <Title>Desgloce:</Title>
@@ -88,14 +96,6 @@ export default function ControlPresupuesto({ presupuesto }) {
                     </Card>
                 </section>
             </div>
-            <main className='w-5/6'>
-                <div className='w-[73%]'>
-                    <ListadoGastos
-                        gastos={gastos}
-                    />
-                </div>
-                <span className='hidden'/>
-            </main>
         </>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header({ presupuesto, setPresupuesto, isValidPresupuesto
   
 
     return (
-        <header className="flex flex-col pt-3  items-center h-screen h-full gap-10 bg-gradient-to-b from-gray-900 to-gray-600 ">
+        <header className="flex flex-col pt-3  max-[640px]:h-auto max-[640px]:p-3 items-center h-screen h-full gap-10 bg-gradient-to-b from-gray-900 to-gray-600 ">
             <section className="flex justify-between px-6 items-center w-full">
                 <div>
                     <h1 className="text-white text-3xl font-semibold">Bienvenido
