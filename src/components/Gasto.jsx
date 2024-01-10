ba3763960efa1c1b9316d1d571006ae8a8627104
Jose Antonio <confi15826s@gmail.com>
2024-01-09 19:43:01 -0600

    swipeable efect agreado a la lista de cambios

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,3 +1,11 @@
+import {
+    LeadingActions,
+    SwipeableList,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions
+} from 'react-swipeable-list'
+import "react-swipeable-list/dist/styles.css"
 import iconoAhorro from '../assets/img/icono_ahorro.svg'
 import iconoComida from '../assets/img/icono_comida.svg'
 import iconoCasa from '../assets/img/icono_casa.svg'
@@ -44,23 +52,46 @@ export default function Gasto({ gasto }) {
         'General': iconoGastoGeneral
     }
 
+    const leadingActions = () => (
+        <LeadingActions >
+            <SwipeAction onClick={() => console.log('editar')}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#fff" d="M5 19h1.425L16.2 9.225L14.775 7.8L5 17.575zm-1 2q-.425 0-.712-.288T3 20v-2.425q0-.4.15-.763t.425-.637L16.2 3.575q.3-.275.663-.425t.762-.15q.4 0 .775.15t.65.45L20.425 5q.3.275.437.65T21 6.4q0 .4-.138.763t-.437.662l-12.6 12.6q-.275.275-.638.425t-.762.15zM19 6.4L17.6 5zm-3.525 2.125l-.7-.725L16.2 9.225z" /></svg>
+            </SwipeAction>
+        </LeadingActions>
+    )
+
+    const trailingActions = () => (
+        <TrailingActions>
+            <SwipeAction onClick={() => console.log('Eliminar')}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#fff" d="M9.808 17h1V8h-1zm3.384 0h1V8h-1zM6 20V6H5V5h4v-.77h6V5h4v1h-1v14z" /></svg>
+            </SwipeAction>
+        </TrailingActions>
+    )
+
 
     return (
-        <div className="w-full max-h-24 flex items-center mt-3 bg-slate-800 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm p-3 ">
-            <img src={diccionarioIconos[gasto.categoria]} alt="netflix" className="max-[640px]:w-16 md:w-20 h-auto" />
+        <SwipeableList >
+            <SwipeableListItem
+                leadingActions={leadingActions()}
+                trailingActions={trailingActions()}
+            >
+                <div className="w-full max-h-24 flex items-center bg-slate-800 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm p-3 ">
+                    <img src={diccionarioIconos[gasto.categoria]} alt="netflix" className="max-[640px]:w-16 md:w-20 h-auto" />
 
-            <div className='flex flex-col w-40 max-[640px]:ml-0 justify-start items-center text-center'>
-                <p className="text-white text-base max-[640px]:text-sm font-bold">{gasto.nombre}</p>
-                <span className=' text-white text-sm max-[640px]:text-xs'>{gasto.categoria}</span>
-            </div>
+                    <div className='flex flex-col w-40 max-[640px]:ml-0 justify-start items-center text-center'>
+                        <p className="text-white text-base max-[640px]:text-sm font-bold">{gasto.nombre}</p>
+                        <span className=' text-white text-sm max-[640px]:text-xs'>{gasto.categoria}</span>
+                    </div>
 
-            <div className='flex justify-end items-center w-full'>
-                <p className="text-white text-xl max-[640px]:text-base font-bold text-center">-{formatearCantidad(gasto.cantidad)}
-                    <br />
-                    <p className="font-semibold text-sm max-[640px]:text-sm text-white">{formatearFecha(gasto.fecha)}</p>
-                </p>
-            </div>
+                    <div className='flex justify-end items-center w-full'>
+                        <p className="text-white text-xl max-[640px]:text-base font-bold text-center">-{formatearCantidad(gasto.cantidad)}
+                            <br />
+                            <span className="font-semibold text-sm max-[640px]:text-sm text-white">{formatearFecha(gasto.fecha)}</span>
+                        </p>
+                    </div>
 
-        </div>
+                </div>
+            </SwipeableListItem>
+        </SwipeableList>
     )
 }
--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -3,7 +3,7 @@ import Gasto from "./Gasto"
 export default function ListadoGastos({ gastos }) {
     return (
         <>
-            <h1 className="text-white text-2xl text-left">{gastos.length ? 'Gastos' : 'Sin gastos'}</h1>
+            <h1 className="text-white text-2xl text-left mb-3">{gastos.length ? 'Gastos' : 'Sin gastos'}</h1>
             {gastos.map(gasto => (
                 <Gasto 
                     key={gasto.id}
